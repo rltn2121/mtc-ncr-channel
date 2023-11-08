227078224188c7cfaeec8534fdda4590929cd9c8
박기수 <rltn2121@naver.com>
2023-11-08 22:44:32 +0900

    sda main mas select test

--- src/test/java/mtc/ncr/channel/db/DBConnectionUtilTest.java
+++ src/test/java/mtc/ncr/channel/db/DBConnectionUtilTest.java
@@ -1,8 +1,6 @@
 package mtc.ncr.channel.db;
 
 import lombok.extern.slf4j.Slf4j;
-import mtc.ncr.channel.db.DBConnectionUtil;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
