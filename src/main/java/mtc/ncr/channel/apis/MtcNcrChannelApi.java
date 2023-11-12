12e5e0e298d40ed6b415a1c8a107baf8a64ec904
Subin Bae <sbsoobin@naver.com>
2023-11-12 12:52:33 +0900

    안쓰는 파일 정리

--- src/main/java/mtc/ncr/channel/apis/MtcNcrChannelApi.java
+++ src/main/java/mtc/ncr/channel/apis/MtcNcrChannelApi.java
@@ -1,19 +1,10 @@
 package mtc.ncr.channel.apis;
 
 import io.swagger.v3.oas.annotations.Operation;
-import mtc.ncr.channel.dto.MtcNcrChannelRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 public interface MtcNcrChannelApi {
-//    @Operation(summary = "#1. Hello API")
-//    @PostMapping("/hello")
-//    ResponseEntity<?> hello(@RequestBody MtcNcrChannelRequest mtcncrchannelrequest);
-//
-//    @Operation(summary = "#2. Hell Get API")
-//    @GetMapping("/hell")
-//    ResponseEntity<?> hell(@ModelAttribute MtcNcrChannelRequest mtcncrchannelrequest);
-
     @Operation(summary = "#1. 전 계좌 조회")
     @GetMapping("/{acno}/all")
     ResponseEntity<?> JohoiAll(@PathVariable("acno") String acno) throws Exception;
