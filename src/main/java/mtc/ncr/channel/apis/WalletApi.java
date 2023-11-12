57635ac34dd7b77a14335935c8c58f3a66bccf9d
Subin Bae <sbsoobin@naver.com>
2023-11-12 17:59:07 +0900

    /wallet 추가

--- src/main/java/mtc/ncr/channel/apis/WalletApi.java
+++ src/main/java/mtc/ncr/channel/apis/WalletApi.java
@@ -7,19 +7,13 @@ import org.springframework.web.bind.annotation.*;
 
 public interface WalletApi {
 
-    ResponseEntity<?> JohoiAll(String acno) throws Exception;
+    @Operation(summary = "#1. 전 계좌 조회")
+    @GetMapping("/{acno}/all")
+    ResponseEntity<?> JohoiAll(@PathVariable("acno") String acno) throws Exception;
 
-    ResponseEntity<?> JohoiByCurC(String acno, String cur_c) throws Exception;
+    @Operation(summary = "#2. 통화코드별  조회")
+    @GetMapping("/{acno}")
+    ResponseEntity<?> JohoiByCurC(@PathVariable("acno") String acno,
+                                  @RequestParam(value = "cur_c") String cur_c) throws Exception;
 
-    public interface MtcNcrChannelApi {
-
-        @Operation(summary = "#1. 전 계좌 조회")
-        @GetMapping("/{acno}/all")
-        ResponseEntity<?> JohoiAll(@PathVariable("acno") String acno) throws Exception;
-
-        @Operation(summary = "#2. 통화코드별  조회")
-        @GetMapping("/{acno}")
-        ResponseEntity<?> JohoiByCurC(@PathVariable("acno") String acno,
-                                      @RequestParam(value = "cur_c") String cur_c) throws Exception;
-    }
 }
